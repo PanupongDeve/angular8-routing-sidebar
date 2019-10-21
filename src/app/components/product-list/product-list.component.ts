b0e39f7c02b8303a0c8ccf63903e2bf2543e169a
Panupong <5635512110@psu.ac.th>
2019-10-21 07:04:55 +0700

    update tutorial

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -22,6 +22,10 @@ export class ProductListComponent implements OnInit {
     window.alert('The product has been shared!');
   }
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
   getVal(): number {
     return 4;
   }
